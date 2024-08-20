3c68008f487925f1be7c1ffaa50e6039596dc242
mohd jami <your_email@example.com>
2024-08-21 05:19:44 +0530

    Fixed Issues UI

--- components/FileComponent.tsx
+++ components/FileComponent.tsx
@@ -50,12 +50,31 @@ export default function FileComponent() {
   };
   return (
     <>
-      <CardTitle className="">Please use only Text Images</CardTitle>
+      <CardTitle className="">
+        <div
+          className="bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 mb-4 flex items-center justify-between"
+          role="alert"
+        >
+          <div>
+            <p className="font-bold">Tip!!</p>
+            <p>Use Images which include Texts.</p>
+          </div>
+          <button className="bg-yellow-200 text-yellow-700 rounded-full p-1 hover:bg-yellow-300 focus:outline-none">
+            <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </CardTitle>
       <div className="grid gap-4 py-8">
         <Label className="text-center text-4xl">
           Image Readability Checker
         </Label>
-        <div className="grid lg:grid-rows-3 gap-4">
+        <div className="grid lg:grid-cols-3 gap-4">
           <form onSubmit={handleSubmit} className="grid lg:grid-rows-2 gap-4">
             <Input name="file" ref={inputFileRef} type="file" required />
             &nbsp;&nbsp;&nbsp;&nbsp;
